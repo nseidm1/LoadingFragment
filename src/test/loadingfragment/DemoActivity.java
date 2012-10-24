836779b95a61a2323f103dcd6ffee81abf0acfaa
nseidman <nseidman@nseidman-w1.roundarch.com>
2012-10-24 11:49:07 -0400

    Cleanup.

--- src/test/loadingfragment/DemoActivity.java
+++ src/test/loadingfragment/DemoActivity.java
@@ -2,9 +2,7 @@ package test.loadingfragment;
 
 import main.loadingfragment.R;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
 
 public class DemoActivity extends FragmentActivity{
     
@@ -12,12 +10,5 @@ public class DemoActivity extends FragmentActivity{
     public void onCreate(Bundle bundle){
 	super.onCreate(bundle);
 	setContentView(R.layout.demo_activity);
-	//Add the DemoFragment programatically.
-	Fragment demoFragment = getSupportFragmentManager().findFragmentByTag(DemoFragment.class.getName());
-	if (demoFragment == null){
-	    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-	    ft.add(R.id.fragment_container, DemoFragment.newInstance(), DemoFragment.class.getName());
-	    ft.commit();
-	}
     }
 }
